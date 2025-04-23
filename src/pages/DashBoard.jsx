f4723fed2428f3e9f23824287864c5b3932b1c7d
Anjana George <anjana7581@gmail.com>
2025-04-23 20:39:20 +0530

    updated StatsCard icons to display in dashboard

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,12 +1,13 @@
 import StatsCard from "../components/StatsCard";
+import { FiUsers, FiMapPin, FiHome, FiActivity } from "react-icons/fi";
+import { FaUmbrellaBeach, FaHotel } from "react-icons/fa";
 
 export default function Dashboard() {
   const stats = [
-    { title: "Users", value: "932", icon: "👥" },
-    { title: "Destinations", value: "754", icon: "🔒" },
-    { title: "Hotels", value: "40", icon: "🔒" },
-    { title: "Adventures", value: "32K", icon: "🔒" },
-
+    { title: "Users", value: "932", icon: <FiUsers className="text-2xl" /> },
+    { title: "Destinations", value: "754", icon: <FiMapPin className="text-2xl" /> },
+    { title: "Hotels", value: "40", icon: <FaHotel className="text-2xl" /> },
+    { title: "Adventures", value: "32K", icon: <FaUmbrellaBeach className="text-2xl" /> },
   ];
 
   return (
