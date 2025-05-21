9ffe99803876af7b0b3ae5fc38cfc4a6cae7f3bd
Anjana George <anjana7581@gmail.com>
2025-05-21 18:34:11 +0530

    updated Pending and Varified Adventure Module

--- src/components/ActionButtons.jsx
+++ src/components/ActionButtons.jsx
@@ -1,8 +1,8 @@
 const ActionButtons = ({ onReject, onApprove }) => (
-    <div className="flex flex-col sm:flex-row justify-end gap-3 pt-4 border-t border-gray-200">
+    <div className="flex flex-col sm:flex-row justify-end gap-3 pt-4 ">
       <button 
         onClick={onReject}
-        className="px-6 py-2 border border-red-500 text-red-500 rounded-full hover:bg-red-50 transition-colors"
+        className="px-6 py-2 border border-[#00493E] text-[#00493E] rounded-full  transition-colors"
       >
         Reject
       </button>
--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,5 +1,5 @@
 const SectionTitle = ({ children }) => (
-    <h2 className="text-lg md:text-xl font-semibold text-[#00493E] mb-2">
+    <h2 className="text-lg md:text-xl font-semibold text-[#303972] mb-2">
       {children} 
     </h2>
   );
