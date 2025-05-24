360db4b6996e00e8df639425db0fef0dd38eacea
Anjana George <anjana7581@gmail.com>
2025-05-24 15:44:22 +0530

    Rebase sucessfully featured/hotels to featured/pending_adventure

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,23 @@ import { AuthProvider } from './context/AuthContext';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import { UserProvider } from './context/UserContext';
-
-import { AdventuresProvider } from './context/AdventuresContext';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
 function App() {
   return (
     <Router>
       <AuthProvider>
-        <>
-          <AppRoutes />
-          <ToastContainer
-            position="top-right"
-            autoClose={3000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="colored"
-          />
-        </>
+        <AppRoutes />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
       </AuthProvider>
     </Router>
   );
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,44 +1,45 @@
 import { Routes, Route, Navigate, Outlet } from "react-router-dom";
+
+// Public Pages
 import Login from "../pages/login";
 import Signup from "../pages/Signup";
+import NotFound from "../pages/NotFound";
+
+// Protected Pages
 import Dashboard from "../pages/DashBoard";
 import Users from "../pages/Users";
-import NotFound from "../pages/NotFound";
+import Pending_Adventures from "../pages/Pending_Adventures";
+import DetailedPending_Adventures from "../pages/DetailedPending_Adventures";
+import Varified_Adventures from "../pages/Varified_Adventures";
+import DetailedVarified_Adventures from "../pages/DetailedVarified_Adventures";
+import EditAdventure from "../pages/EditAdventure";
+import AdventuresBooking from "../pages/AdventuresBooking";
+import Detailed_advantures from "../pages/Detailed_Advantures_Booking";
+import Destination from "../pages/Destination";
+import DestinationDetail from "../pages/Detailed_Destination";
+import DestinationForm from "../components/DestinationForm";
+import EditDestination from "../pages/EditDestination";
+import Pending_Hotels from "../pages/Pending_Hotels";
+import DetailedPending_Hotels from "../pages/DetailedPending_Hotels";
+import Varified_Hotels from "../pages/Varified_Hotels";
+import DetailedVarified_Hotels from "../pages/DetailedVarified_Hotels";
+import EditHotel from "../pages/EditHotel";
+import HotelBookings from "../pages/HotelBookings";
+import DetailedHotelBookings from "../pages/DetailedHotelBooking";
+import Settings from "../pages/Settings";
+import Profile from "../pages/Profile";
+
+// Layout and Route Guards
 import ProtectedRoute from "../components/ProtectedRoute";
 import RoleBasedRoute from "../components/RoleBasedRoute";
 import DashboardLayout from "../Layouts/DashboardLayout";
-import Pending_Adventures from "../pages/Pending_Adventures";
-import DetailedPending_Adventures from "../pages/DetailedPending_Adventures";
-import Detailed_advantures from "../pages/Detailed_Advantures_Booking";
-import Destination from "../pages/Destination";
-import Settings from "../pages/Settings";
-import Profile from "../pages/Profile";
-import Varified_Adventures from "../pages/Varified_Adventures";
-import DetailedVarified_Adventures from "../pages/DetailedVarified_Adventures";
-import AdventuresBooking from "../pages/AdventuresBooking";
-import EditAdventure from "../pages/EditAdventure";
-import Pending_Hotels from "../pages/Pending_Hotels";
-import Varified_Hotels from "../pages/Varified_Hotels";
-import DetailedPending_Hotels from "../pages/DetailedPending_Hotels";
-import DetailedVarified_Hotels from "../pages/DetailedVarified_Hotels";
-// import Destinations from "../pages/Destinations";
-import HotelBookings from "../pages/HotelBookings";
-import DetailedHotelBookings from "../pages/DetailedHotelBooking";
-import EditHotel from "../pages/EditHotel";
 
-import DestinationDetail from "../pages/Detailed_Destination";
-import DestinationForm from "../components/DestinationForm";
-import EditDestination from "../pages/EditDestination"
-import DestinationDetail from "../pages/Detailed_Destination";
-import DestinationForm from "../components/DestinationForm";
-import EditDestination from "../pages/EditDestination"
-// Protected Layout Wrapper
+// Protected layout wrapper
 const ProtectedLayout = () => (
-
   <ProtectedRoute>
     <RoleBasedRoute allowedRoles={["admin"]}>
       <DashboardLayout>
-        <Outlet /> {/* This renders the nested routes */}
+        <Outlet />
       </DashboardLayout>
     </RoleBasedRoute>
   </ProtectedRoute>
@@ -47,56 +48,48 @@ const ProtectedLayout = () => (
 const AppRoutes = () => {
   return (
     <Routes>
-      {/* Redirect root path to /login */}
+      {/* Redirect root to login */}
       <Route path="/" element={<Navigate to="/login" />} />
 
-      {/* PUBLIC ROUTES */}
+      {/* Public Routes */}
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
 
-      {/* PROTECTED ROUTES */}
+      {/* Protected Routes */}
       <Route element={<ProtectedLayout />}>
-        {/* Dashboard */}
         <Route path="/dashboard" element={<Dashboard />} />
-        
-        {/* User Management */}
         <Route path="/users" element={<Users />} />
-        
+
         {/* Adventures */}
         <Route path="/pending-adventures" element={<Pending_Adventures />} />
         <Route path="/pending-adventures/:id" element={<DetailedPending_Adventures />} />
-        <Route path="/verified-adventures" element={<Varified_Adventures/>} />
-        <Route path="/verified-adventures/:id" element={<DetailedVarified_Adventures/>} />
-        <Route path="/edit-adventure/:id" element={<EditAdventure/>} />
+        <Route path="/verified-adventures" element={<Varified_Adventures />} />
+        <Route path="/verified-adventures/:id" element={<DetailedVarified_Adventures />} />
+        <Route path="/edit-adventure/:id" element={<EditAdventure />} />
+        <Route path="/adventure-bookings" element={<AdventuresBooking />} />
+        <Route path="/adventure-bookings/:id" element={<Detailed_advantures />} />
 
         {/* Destinations */}
-        <Route path="/destination" element={<Destination/>} />
-        <Route path="/destination/:id" element={<DestinationDetail/>} />
-        <Route path="/destination/new" element={<DestinationForm/> } />
-        <Route path="/destination/edit/:id" element={<EditDestination/>} />
-        <Route path="/destination/:id" element={<DestinationDetail/>} />
-        
-        {/* Hotels */}
-        <Route path="/pending-hotels" element={<Pending_Hotels/>} />
-        <Route path="/pending-hotels/:id" element={<DetailedPending_Hotels/>} />
-        <Route path="/verified-hotels" element={<Varified_Hotels/>} />
-        <Route path="/verified-hotels/:id" element={<DetailedVarified_Hotels/>} />
-        <Route path="/edit-hotel/:id" element={<EditHotel/>} />
+        <Route path="/destination" element={<Destination />} />
+        <Route path="/destination/new" element={<DestinationForm />} />
+        <Route path="/destination/edit/:id" element={<EditDestination />} />
+        <Route path="/destination/:id" element={<DestinationDetail />} />
 
-        
-        {/* Bookings */}
+        {/* Hotels */}
+        <Route path="/pending-hotels" element={<Pending_Hotels />} />
+        <Route path="/pending-hotels/:id" element={<DetailedPending_Hotels />} />
+        <Route path="/verified-hotels" element={<Varified_Hotels />} />
+        <Route path="/verified-hotels/:id" element={<DetailedVarified_Hotels />} />
+        <Route path="/edit-hotel/:id" element={<EditHotel />} />
         <Route path="/hotel-bookings" element={<HotelBookings />} />
         <Route path="/hotel-bookings/:id" element={<DetailedHotelBookings />} />
 
-         <Route path="/adventure-bookings" element={<AdventuresBooking/>} /> 
-         <Route path="/adventure-bookings/:id" element={<Detailed_advantures/>} /> 
-        
-        {/* Settings & Profile */}
+        {/* Profile & Settings */}
         <Route path="/settings" element={<Settings />} />
         <Route path="/profile" element={<Profile />} />
       </Route>
 
-      {/* 404 PAGE */}
+      {/* Catch-All Route */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
