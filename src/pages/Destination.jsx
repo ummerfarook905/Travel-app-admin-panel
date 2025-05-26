c2f6fb455d2e64750dcd3c50de6e70cbf5cf161d
Anjana George <anjana7581@gmail.com>
2025-05-26 12:46:37 +0530

    delete duplication in Destination

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -12,27 +12,16 @@ const Destionation =()=>{
 
   return(
     <div className ="p-6 bg-[#f6f8fc] min-h-screen">
-      <div className ="flex justify-between items-center mb-6">
-        <h1 className="text-2xl font-bold text-gray-800">Destinations</h1>
+      <div className="flex justify-between items-center mb-6">
+        <div></div>  {/* spacer */}
         <button 
-        onClick={()=>navigate('/destination/new')}
-        className="bg-[#004d40] text-white px-5 py-2 rounded-full hover:bg-[#00332c] transition">
-        <FaPlus className="inline mr-2" /> New Destination
-
+          onClick={() => navigate('/destination/new')}
+          className="bg-[#004d40] text-white px-5 py-2 rounded-full hover:bg-[#00332c] transition"
+        >
+          <FaPlus className="inline mr-2" /> New Destination
         </button>
       </div>
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
-        {destinations.map((dest,index)=>(
-          <DestinationCard 
-          key={index} 
-          name={dest.name}
-          image={dest.image}
-          onClick={()=>navigate(`/destination/${dest.id}`)}
-          />
-        ))}
-
-      </div>
 
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
         {destinations.map((dest,index)=>(
@@ -45,6 +34,7 @@ const Destionation =()=>{
         ))}
 
       </div>
+
     </div>
   )
 }
