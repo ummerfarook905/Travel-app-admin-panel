1d5848380f40464095a6b6442e7fc2c2be3e402c
Anjana George <anjana7581@gmail.com>
2025-06-03 01:34:04 +0530

    implemented lazy loading

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,7 +3,7 @@ import NotFound from "../pages/NotFound";
 import ProtectedRoute from "../components/ProtectedRoute";
 import RoleBasedRoute from "../components/RoleBasedRoute";
 import DashboardLayout from "../Layouts/DashboardLayout";
-
+import { Suspense } from "react";
 import { publicRoutes, protectedRoutes } from "./routesConfig";
 
 const ProtectedLayout = () => (
@@ -18,29 +18,38 @@ const ProtectedLayout = () => (
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      {/* Default Redirect */}
-      <Route path="/" element={<Navigate to="/login" />} />
+    <Suspense
+      fallback={
+        <div className="flex justify-center items-center h-screen">
+          <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-green-900"></div>
+          <p className="ml-4 text-gray-700 text-lg">Loading, please wait...</p>
+        </div>
+      }
+    >
+        <Routes>
+          {/* Default Redirect */}
+          <Route path="/" element={<Navigate to="/login" />} />
 
 
-      {/* Public Routes */}
-      {publicRoutes.map((route, index) => (
-        <Route key={index} path={route.path} element={route.element} />
-      ))}
+          {/* Public Routes */}
+          {publicRoutes.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} />
+          ))}
 
 
-      {/* Protected Routes under ProtectedLayout */}
-      <Route element={<ProtectedLayout />}>
-        {protectedRoutes.map((route, index) => (
-          <Route key={index} path={route.path} element={route.element} />
-        ))}
-      </Route>
+          {/* Protected Routes under ProtectedLayout */}
+          <Route element={<ProtectedLayout />}>
+            {protectedRoutes.map((route, index) => (
+              <Route key={index} path={route.path} element={route.element} />
+            ))}
+          </Route>
 
 
-      {/* 404 */}
-      <Route path="*" element={<NotFound />} />
-      
-    </Routes>
+          {/* 404 */}
+          <Route path="*" element={<NotFound />} />
+          
+        </Routes>
+    </Suspense>
   );
 };
 
